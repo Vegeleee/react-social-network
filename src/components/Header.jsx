3ec62a923b7affd371c049b4457d4901dc8ef718
Vegeleee <danil_vegele@mail.ru>
2019-10-22 02:49:41 +0200

    Add base structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://www.weddingprofessionalsofcolumbus.com/wp-content/uploads/logo-ex-7.png' />
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<section className='content'>
+			<div>
+				<img src='http://www.sandybeachinternational.com/wp-content/uploads/2018/11/cropped-beach-exotic-holiday-248797.jpg' />
+			</div>
+			<div>
+				ava + desc
+			</div>
+			<div>
+				my posts
+				<div>
+					new post
+				</div>
+				<div>
+					<div>
+						post 1
+					</div>
+					<div>
+						post 2
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Profile;
