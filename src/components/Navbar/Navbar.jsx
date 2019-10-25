e187b739ee89ef2eb35f68cc0c896a58ca1d3993
Vegeleee <danil_vegele@mail.ru>
2019-10-25 16:33:13 +0200

    Routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
 	return (
 		<nav className={classes.nav}>
 			<ul>
-				<li className={classes.item}>Messages</li>
-				<li className={classes.item}>Profile</li>
-				<li className={classes.item}>News</li>
-				<li className={classes.item}>Music</li>
-				<li className={classes.item}></li>
+				<li className={classes.item}><a href='/profile'>Profile</a></li>
+				<li className={classes.item}><a href='/dialogs'>Messages</a></li>
+				<li className={classes.item}><a href='/news'>News</a></li>
+				<li className={classes.item}><a href='/music'>Music</a></li>
+				<li className={classes.item}><a href='/settings'>Settings</a></li>
 			</ul>
 		</nav>
 	);
