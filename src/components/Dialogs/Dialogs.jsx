c88fdb6a6246212e1855bac704b91e0fd27a1101
Vegeleee <danil_vegele@mail.ru>
2019-10-26 04:43:09 +0200

    Dialogs page strusture created

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,20 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
 	return (
-		<div>
-			Dialogs
+		<div className={classes.dialogs}>
+			<div className={classes.dialogItems}>
+				<div className={`${classes.dialog} ${classes.active}`}>Danil</div>
+				<div className={classes.dialog}>Slava</div>
+				<div className={classes.dialog}>Maria</div>
+				<div className={classes.dialog}>Sasha</div>
+				<div className={classes.dialog}>Aleksey</div>
+				<div className={classes.dialog}>Vlad</div>
+			</div>
+			<div className={classes.messages}>
+				<div className={classes.message}>Hi</div>
+				<div className={classes.message}>How are you</div>
+				<div className={classes.message}>Yo!</div>
+			</div>
 		</div>
 	);
 };
