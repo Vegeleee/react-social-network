b5b1f64c1b654463658759c193da5cab332e121f
Vegeleee <danil_vegele@mail.ru>
2019-11-02 20:08:40 +0200

    Ref

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -13,6 +13,13 @@ const Dialogs = (props) => {
 	let messagesElements = props.state.messages
 		.map(m => <Message message={m.message} id={m.id} />);
 
+	let newMessageElement = React.createRef();
+
+	let sendMessage = () => {
+		let text = newMessageElement.current.value;
+		alert(text);
+	};
+
 	return (
 		<div className={classes.dialogs}>
 			<div className={classes.dialogItems}>
@@ -20,6 +27,12 @@ const Dialogs = (props) => {
 			</div>
 			<div className={classes.messages}>
 				{messagesElements}
+				<div>
+					<textarea ref={newMessageElement}></textarea>
+				</div>
+				<div>
+					<button onClick={sendMessage}>Send</button>
+				</div>
 			</div>
 		</div>
 	);
