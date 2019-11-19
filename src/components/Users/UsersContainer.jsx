8cc43a86a1e02d1ce8b6d36f73184ef838600c21
Vegeleee <danil_vegele@mail.ru>
2019-11-19 03:33:11 +0200

    Simple pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Users from './Users';
-import { followAC, unfollowAC, setUsersAC } from '../../redux/users-reducer';
+import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setUsersTotalCountAC } from '../../redux/users-reducer';
 
 const mapStateToProps = state =>
 	({
-		users: state.usersPage.users
+		users: state.usersPage.users,
+		pageSize: state.usersPage.pageSize,
+		totalUsersCount: state.usersPage.totalUsersCount,
+		currentPage: state.usersPage.currentPage
 	});
 
 const mapDispatchToProps = dispatch =>
@@ -18,6 +21,12 @@ const mapDispatchToProps = dispatch =>
 		},
 		setUsers(users) {
 			dispatch(setUsersAC(users));
+		},
+		setCurrentPage(currentPage) {
+			dispatch(setCurrentPageAC(currentPage));
+		},
+		setTotalUsersCount(totalUsersCount) {
+			dispatch(setUsersTotalCountAC(totalUsersCount));
 		}
 	});
 
