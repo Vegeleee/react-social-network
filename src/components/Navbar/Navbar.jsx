d146fa352a152a4313788481d5de5ba14ebc17c6
Vegeleee <danil_vegele@mail.ru>
2020-01-27 03:56:14 +0200

    Navbar markup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import classes from './Navbar.module.css';
+import classes from './Navbar.module.scss';
 import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
 	return (
 		<nav className={classes.nav}>
-			<ul>
-				<li className={classes.item}><NavLink to='/profile' activeClassName={classes.active}>Profile</NavLink></li>
-				<li className={classes.item}><NavLink to='/dialogs' activeClassName={classes.active}>Messages</NavLink></li>
-				<li className={classes.item}><NavLink to='/users' activeClassName={classes.active}>Users</NavLink></li>
-				<li className={classes.item}><NavLink to='/news' activeClassName={classes.active}>News</NavLink></li>
-				<li className={classes.item}><NavLink to='/music' activeClassName={classes.active}>Music</NavLink></li>
-				<li className={classes.item}><NavLink to='/settings' activeClassName={classes.active}>Settings</NavLink></li>
+			<ul className={classes.navList}>
+				<li className={classes.navListItem}><NavLink to='/profile' activeClassName={classes.active}>Profile</NavLink></li>
+				<li className={classes.navListItem}><NavLink to='/dialogs' activeClassName={classes.active}>Messages</NavLink></li>
+				<li className={classes.navListItem}><NavLink to='/users' activeClassName={classes.active}>Users</NavLink></li>
+				<li className={classes.navListItem}><NavLink to='/news' activeClassName={classes.active}>News</NavLink></li>
+				<li className={classes.navListItem}><NavLink to='/music' activeClassName={classes.active}>Music</NavLink></li>
+				<li className={classes.navListItem}><NavLink to='/settings' activeClassName={classes.active}>Settings</NavLink></li>
 			</ul>
 		</nav>
 	);
