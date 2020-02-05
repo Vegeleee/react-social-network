92becc80258d9cc16ddd65147baee4fe36e848aa
Vegeleee <danil_vegele@mail.ru>
2020-02-05 02:16:12 +0200

    Preloader css

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import preloader from '../../../assets/images/preloader.gif';
+import classes from './Preloader.module.scss';
 
 
 const Preloader = () => {
 	return (
-		<div>
+		<div className={classes.preloader}>
 			<img src={preloader} />
 		</div>
 	);
