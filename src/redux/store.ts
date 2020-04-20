b606d97e886eb4cb81bb4a9790e06f593345c684
Vegeleee <danil_vegele@mail.ru>
2020-04-20 22:49:14 +0200

    Add type for actions

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,11 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux"
-import profileReducer from "./profile-reducer"
-import dialogsReducer from "./dialogs-reducer"
-import usersReducer from "./users-reducer"
-import authReducer from "./auth-reducer"
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import profileReducer from './profile-reducer'
+import dialogsReducer from './dialogs-reducer'
+import usersReducer from './users-reducer'
+import authReducer from './auth-reducer'
 import thunkMiddleware from 'redux-thunk'
-import {reducer as formReducer} from 'redux-form'
-import appReducer from "./app-reducer"
+import { reducer as formReducer } from 'redux-form'
+import appReducer from './app-reducer'
 
 const rootReducer = combineReducers({
 	profilePage: profileReducer,
@@ -13,16 +13,23 @@ const rootReducer = combineReducers({
 	usersPage: usersReducer,
 	auth: authReducer,
 	form: formReducer,
-	app: appReducer
-});
+	app: appReducer,
+})
 
 type RootReducerType = typeof rootReducer
 export type AppStateType = ReturnType<RootReducerType>
 
+type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never
+
+export type InferActionTypes<
+	T extends { [key: string]: (...args: any[]) => any }
+> = ReturnType<PropertiesTypes<T>>
+
 // @ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunkMiddleware)
-  ));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunkMiddleware))
+)
 
 export default store
