6a12717b440a8049bbd06145208b869c857b262a
Vegeleee <danil_vegele@mail.ru>
2020-05-20 18:02:53 +0200

    Preloader types

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import preloader from '../../../assets/images/preloader.gif'
 import classes from './Preloader.module.scss'
 
-
-const Preloader = () => {
+const Preloader: React.FC = () => {
 	return (
-		<div className={classes.preloader} >
-			<img src={preloader} alt='Preloader' />
+		<div className={classes.preloader}>
+			<img src={preloader} alt="Preloader" />
 		</div>
 	)
 }
